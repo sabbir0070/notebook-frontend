06ec9497c884eab197932f49d9a42d770d9a8bca
sabbir0070 <mdsharifulalamsiddique79672@gmail.com>
2026-04-09 03:23:46 +0600

    fix

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Update base URL depending on environment if needed
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://notebook-server-gilt.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
